a418165c0867f8194baf3a5ced2d5e70d4efd25c
Nutan Nimkar <nnimk038@uottawa.ca>
2020-07-23 23:20:10 -0400

    Added name and date

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#Author: Nutan Nimkar
+#Date:   23rd July 2020
 #All the imports required to run the app and its functions
 from flask import Flask, render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
